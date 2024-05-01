a470ea78d212e963eddda0a6bd08758088cae29e
mjwSolver <mwiradinata@student.ciputra.ac.id>
2024-05-01 16:40:29 +0700

    Updating the Position of Every View Element

--- RectangularOCR/RecognizeTextViewController.swift
+++ RectangularOCR/RecognizeTextViewController.swift
@@ -15,26 +15,34 @@ class RecognizeTextViewController: UIViewController {
     private let selectedImageView: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "photographer-bg")
-        
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 16.0
         return image
     }()
     
     private let extractedDimensionsTitleLabel: UILabel = {
         let label = UILabel()
-        label.font.withSize(15)
+        label.textAlignment = .center
+//        label.font.withSize(15)
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
         label.text = "Extracted Dimensions"
         return label
     }()
     
     private let extractedDimensionsLabel: UILabel = {
         let label = UILabel()
-        label.font.withSize(23)
+        label.textAlignment = .center
+//        label.font.withSize(23)
+        label.font = UIFont.systemFont(ofSize: 23, weight: .semibold)
         label.text = "? X ?"
+        
         return label
     }()
     
     private let calculatedAreaTitleLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
         label.font.withSize(15)
         label.text = "Calculated Area"
         return label
@@ -42,6 +50,7 @@ class RecognizeTextViewController: UIViewController {
     
     private let calculatedAreaLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
         label.font.withSize(23)
         label.text = "? meter"
         return label
@@ -60,36 +69,48 @@ class RecognizeTextViewController: UIViewController {
     
     private func setUpUI(){
         
+        let leftMargin = 20.0
+        let standardWidth = view.frame.size.width - leftMargin*2
+        
+        let selectedImageIsValid = selectedImage != nil
+        selectedImageView.image = selectedImageIsValid ? selectedImage : UIImage(named: "photographer-bg")
+        
+        if(selectedImageIsValid) {
+            recognizeTextFromImage(image: selectedImage)
+        }
+        
         self.view.addSubview(selectedImageView)
         selectedImageView.frame = CGRect(x: 20,
-                             y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
-                             height: 200)
+                                         y: 100,
+                                         width: standardWidth,
+                                         height: 250)
         
         self.view.addSubview(extractedDimensionsTitleLabel)
         extractedDimensionsTitleLabel.frame = CGRect(x: 20,
-                             y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
-                             height: 200)
+                                                     y: 400,
+                                                     width: standardWidth,
+                                                     height: 50)
+        
+        extractedDimensionsTitleLabel.layer.borderColor = CGColor(red: 255, green: 0, blue: 0, alpha: 1.0)
+        extractedDimensionsTitleLabel.layer.borderWidth = 2
         
         self.view.addSubview(extractedDimensionsLabel)
         extractedDimensionsLabel.frame = CGRect(x: 20,
-                             y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
-                             height: 200)
+                                                 y: 428,
+                                                 width: standardWidth,
+                                                 height: 50)
         
         self.view.addSubview(calculatedAreaTitleLabel)
         calculatedAreaTitleLabel.frame = CGRect(x: 20,
-                             y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
-                             height: 200)
+                                             y: 500,
+                                             width: standardWidth,
+                                             height: 50)
         
         self.view.addSubview(calculatedAreaLabel)
         calculatedAreaLabel.frame = CGRect(x: 20,
-                             y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
-                             height: 200)
-
+                                             y: 528,
+                                             width: standardWidth,
+                                             height: 50)
         
         
     }
