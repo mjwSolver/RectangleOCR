78e03e9a9c8ccd5901dfe4298f58091fa3f548a0
mjwSolver <mwiradinata@student.ciputra.ac.id>
2024-05-01 15:07:24 +0700

    Finalizing the View Elements for ViewController

--- RectangularOCR/ViewController.swift
+++ RectangularOCR/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
     
     private let label: UILabel = {
         let label = UILabel()
-        
+        label.numberOfLines = 2
         label.textAlignment = .center
-        label.font.withSize(10)
+        label.font.withSize(20)
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
         label.textColor = .white
         label.text = "Start calculating the area of your rectangle!"
         return label
@@ -29,13 +30,18 @@ class ViewController: UIViewController {
         config.baseBackgroundColor = .systemBlue
         config.baseForegroundColor = .white
         
-        config.imagePadding = 5
+        config.imagePadding = 4
         config.image = UIImage(systemName: "square.and.arrow.up.on.square")
         config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 14, 
                                                                                   weight: .medium,
                                                                                   scale: .default)
         
         config.attributedTitle = "Import an Image"
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.preferredFont(forTextStyle: .body)
+            return outgoing
+        }
         
         button.configuration = config
         
@@ -62,49 +68,44 @@ class ViewController: UIViewController {
         view.addSubview(backgroundImageView)
         view.addSubview(label)
         view.addSubview(importImageButton)
-        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        backgroundImageView.frame = CGRect(x: 0, y: 0,
-                                 width: UIScreen.main.bounds.width,
-                                 height: UIScreen.main.bounds.height)
-        
-        label.frame = CGRect(x: 20,
+        label.frame = CGRect(x: 30,
                              y: view.frame.size.width + view.safeAreaInsets.top,
-                             width: view.frame.size.width-40,
+                             width: view.frame.size.width-60,
                              height: 300)
-        
         
         // MARK: Go to recognize textview after pressing the image button
         importImageButton.addTarget(self, action: #selector(imageTapped), for:.touchUpInside)
         
-        importImageButton.frame = CGRect(
-                                        x: view.frame.size.width / 3,
-                                        y: view.frame.size.height - 200,
-                                        width: 190,
-                                        height: 55)
+        importImageButton.translatesAutoresizingMaskIntoConstraints = false
         
-        importImageButton.center = self.view.center
+        NSLayoutConstraint.activate([
+            // Center horizontally
+            importImageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // Set distance from bottom
+            importImageButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            // Set width
+            importImageButton.widthAnchor.constraint(equalToConstant: 190),
+            // Set height
+            importImageButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
         
         
-    }
-    
-    
-    @objc func tapImageThenGoToRecognizeTextView() {
         
-        imageTapped()
-        goToRecognizeTextView()
+        backgroundImageView.frame = CGRect(x: 0, y: 0,
+                                 width: UIScreen.main.bounds.width,
+                                 height: UIScreen.main.bounds.height)
         
     }
     
     // MARK: - Add TapGasture In ImageView
     @objc func goToRecognizeTextView() {
         let vc = RecognizeTextViewController()
-//        self.navigationController?.pushViewController(vc, animated: true)
-        present(vc, animated: true, completion: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
     }
         
     @objc func imageTapped() {
@@ -122,7 +123,13 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo
                                info: [UIImagePickerController.InfoKey : Any]) {
         
-        backgroundImageView.image = info[.originalImage] as? UIImage
+        let theSelectedImage = info[.originalImage] as? UIImage
+        backgroundImageView.image = theSelectedImage
+        
+        let vc = RecognizeTextViewController()
+        vc.selectedImage = theSelectedImage
+        navigationController?.pushViewController(vc, animated: true)
+        
         dismiss(animated: true, completion: nil)
     }
     
